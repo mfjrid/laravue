1ddd0a08e2056bd33c2d40b3d1c0ac07ec8618a6
fajar <helpdesk.aplikasi6@kpk.go.id>
2023-02-27 16:13:47 +0700

    Menambahkan loading animation

--- vue-request/src/views/ProgressView.vue
+++ vue-request/src/views/ProgressView.vue
@@ -1,4 +1,9 @@
 <template>
+
+  <div class="vl-parent">
+    <loading v-model:active="isLoading" :can-cancel="false" :is-full-page="fullPage" />
+  </div>
+
   <div class="container pt-md-5">
     <div class="row">
       <div class="col-md-12 text-center">
@@ -39,31 +44,52 @@
   </div>
 </template>
 
+<style>
+  .vl-overlay .v1-background {
+    background-color: #000000 !important;
+  }
+
+  .v1-icon svg {
+    stroke: #FF6F00 !important;
+  }
+</style>
+
 <script>
   import axios from 'axios'
+  import Loading from 'vue-loading-overlay';
+  import 'vue-loading-overlay/dist/css/index.css';
 
   export default {
     data() {
       return {
-        number: 1,
         requests: null,
+        isLoading: true,
+        fullPage: true,
       }
+    },
+    components: {
+      Loading
     },
     created() {
       axios
         .get('http://localhost:8000/v1/requests/')
         .then((response) => {
+          this.isLoading = false;
           this.requests = response.data.data.data;
         })
         .catch((e) => {
           this.errors.push(e);
-        }).finally(() => {
-          this.loading = false
-        });
+        })
     }
   }
 </script>
 
 <style>
+  .vl-overlay .vl-background {
+    background-color: #000000;
+  }
 
+  .vl-icon svg {
+    stroke: #FF6F00;
+  }
 </style>
